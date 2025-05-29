0f698ba8c7da36278b7bfdb46934d215a5084c88
Faten Eid <mulan.tech.914h@gmail.com>
2025-05-29 05:19:34 +0300

    remove unnecessary lines and comments

--- 10-oop-as-it-should-be-concepts.cpp
+++ 10-oop-as-it-should-be-concepts.cpp
@@ -1,37 +1,14 @@
 #include <iostream>
-#include "L02_ClassesAndObjects.h"
-#include "L05_AccessSpecifiers.h"
-#include "L06_PropertiesSetAndGet.h"
-#include "L07_ReadOnlyProperty.h"
-#include "L08_PropertiesSetAndGetThroughEquals.h"
-#include "Project1_Calculator.h"
-#include "L13_Constructors.h"
-#include "L15_Destructors.h"
-#include "L16_StaticMembers.h"
-#include "L17_StaticMethods.h"
-#include "Exercise1_Person.h"
-#include "Exercise2_Employee.h"
-#include "Exercise2_EmployeeWithInheritance.h"
-#include "Exercise3_Developer.h"
-#include "Homework_NestedClasses.h"
-#include "L_SeparateClassesInLibraries.h"
-#include "Project2_StringLibrary.h"
-#include "Project3_DateLibrary.h"
 #include "Project4_PeriodClass.h"
 
 using namespace std;
-
 
 int main()
 {
 
 	Project4_PeriodClass::Task();
 
-
-
-
 	return 0;
-
 
 }
 
